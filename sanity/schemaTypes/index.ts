76849425c6d4bfb347bac28e41b351657f8db3cc
otyt-in <quan5288@duck.com>
2026-06-28 17:03:15 +0530

    feat(cms): add footer and seo schemas

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -2,5 +2,7 @@ import { homePage } from "./homePage";
 import { siteSettings } from "./siteSettings";
 import { story } from "./story";
 import { navigation } from "./navigation";
+import { footer } from "./footer";
+import { seo } from "./seo";
 
-export const schemaTypes = [siteSettings, navigation, homePage, story];
+export const schemaTypes = [siteSettings, navigation, footer, seo, homePage, story];
